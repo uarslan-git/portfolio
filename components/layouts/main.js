39254ea045168d2cfa218c10103fe472a0e1c63b
Umut Arslan <umut_arslan@gmx.de>
2025-03-21 17:35:53 +0100

    Add navbar

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,7 @@
 import { Box, Container } from '@chakra-ui/react'
 import Head from 'next/head'
 import React from 'react'
+import Navbar from '../navbar'
 
 const Layout = ({router, children}) => {
   return (
@@ -9,6 +10,7 @@ const Layout = ({router, children}) => {
         <meta name="viewport" content="width=device-width, intial-scale=1"/>
         <title>Umut Arslan - Website</title>
       </Head>
+      <Navbar/>
     <Container maxW="container.md" pt={14}>
       {children}
     </Container>
---
+++ components/navbar.js
@@ -0,0 +1,17 @@
+import { Container } from '@chakra-ui/react'
+import React from 'react'
+import Logo from './logo'
+
+const LinkItem = ({})=> {
+  
+}
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Logo/>
+    </Container>
+  )
+}
+
+export default Navbar
