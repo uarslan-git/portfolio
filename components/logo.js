fdf360ba2fb24541899517e1c9170dcb9f3be7d8
Umut Arslan <umut_arslan@gmx.de>
2025-04-30 10:29:10 +0200

    add logo

--- components/logo.js
+++ components/logo.js
@@ -1,29 +1,29 @@
-import { Link, Text, useColorModeValue } from '@chakra-ui/react'
-import styled from '@emotion/styled'
-import React from 'react'
+import { Link, Text, useColorModeValue } from "@chakra-ui/react"
+import styled from "@emotion/styled"
+import React from "react"
 
 const LogoBox = styled.span`
-font-weight: bold;
-font-size: 18px;
-display: inline-flex;
-height: 30px;
-line-height: 20px;
-align-items: center;
-padding: 10px;
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  height: 30px;
+  line-height: 20px;
+  align-items: center;
+  padding: 10px;
 
-&:hover {
-  transform: rotate(20deg);
-  transition: transform 0.5s;
-}
+  &:hover {
+    transform: rotate(20deg);
+    transition: transform 0.5s;
+  }
 `
 
 const Logo = () => {
   return (
-    <Link href='/'>
+    <Link href="/">
       <LogoBox>
         {/* Inser Logo here */}
         <Text
-          color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+          color={useColorModeValue("gray.800", "whiteAlpha.900")}
           fontFamily="M PLUS Rounded 1c"
           fontWeight="bold"
           ml={3}
