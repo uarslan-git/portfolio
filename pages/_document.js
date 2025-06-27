f40ab7c2eed1353baeba6e3bf75b69d80a896761
Umut Arslan <umut_arslan@gmx.de>
2025-06-27 13:37:47 +0200

    wip

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import { ColorModeScript } from "@chakra-ui/react";
-import NextDocument, {Html, Head, Main, NextScript } from "next/document.js";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import theme from "../libs/theme.js";
 
 export default class Document extends NextDocument {
